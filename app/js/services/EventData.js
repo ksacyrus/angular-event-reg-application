759ba8a59de4efbd2d8a7cc396cec307fc2522f5
Kirill Larionov <kirill.larionov@visma.com>
2016-08-17 14:12:38 +0200

    Removing Webstorm and IIS folders

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,13 +1,13 @@
 eventsApp.factory('eventData', function($http, $resource) {
-    var resourse = $resource('/data/event/:id', {id:'@id'});
+    var resource = $resource('/data/event/:id', {id:'@id'});
     return {
         getEvent: function () {
             // return $http({method: 'GET', url: '/data/event/1'});
-            return resourse.get({id:1});
+            return resource.get({id:1});
         },
         save: function(event) {
             event.id = 999;
-            return resourse.save(event);
+            return resource.save(event);
         }
     };
 });
