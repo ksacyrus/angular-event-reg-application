cc378204047e9b307ed5856c3f5168339896b6eb
Kirill Larionov <kirill.larionov@visma.com>
2016-08-19 16:32:41 +0200

    More Routing and Brower History

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,17 @@
 'use strict';
 
+var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider.when('/newEvent',
+        {
+            templateUrl: 'templates/NewEvent.html',
+            controller: 'EditEventController'
+        })
+    });
+
+/*
 var eventsApp = angular.module('eventsApp', ['ngSanitize', 'ngResource', 'ngCookies'])
     .factory('myCache', function ($cacheFactory) {
         return $cacheFactory('myCache', {capacity:3});
     });
+*/
